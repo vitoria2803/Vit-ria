263ee49608b61f6c6a5d6d2e558994660112f2db
Vitória <viana.souza.vitoria@escola.pr.gov.br>
2025-06-26 08:43:30 -0300

    ok

---
+++ script.js
@@ -0,0 +1,5 @@
+function inserirNome(){
+   let nomeUsuario = prompt("Qual o seu nome");
+   let elemento = document.querySelector("#nome-usuario");
+   elemento.textContent = nomeUsuario;
+   inserirNome();
