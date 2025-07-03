dd7053cbe1e9bba6643e50b00c01dec5ace3a170
Vitória <viana.souza.vitoria@escola.pr.gov.br>
2025-07-03 08:05:30 -0300

    ok

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function inserirNome(){
    let nomeUsuario = prompt("Qual o seu nome");
    let elemento = document.querySelector("#nome-usuario");
+   console.log(elemento);
    elemento.textContent = nomeUsuario;
-   inserirNome();
+}
+
+inserirNome();
